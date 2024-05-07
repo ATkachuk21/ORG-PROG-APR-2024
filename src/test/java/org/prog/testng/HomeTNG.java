d01c3e89f6c6904fbf0081c8edc5cebe2993b013
Tkachuk21 <tkachuk210399@gmail.com>
2024-05-07 18:27:26 +0300

    HomeTNG

---
+++ src/test/java/org/prog/testng/HomeTNG.java
@@ -0,0 +1,12 @@
+package org.prog.testng;
+
+import org.testng.annotations.Test;
+
+public class HomeTNG {
+
+        @Test
+        public void demo () {
+            System.out.println("Hello Andrii");
+        }
+    }
+
