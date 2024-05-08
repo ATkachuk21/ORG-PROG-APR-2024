be7465e79e7ea9cd30a72f317f11716773afc38b
Tkachuk21 <tkachuk210399@gmail.com>
2024-05-08 14:32:03 +0300

    HomeWork

---
+++ src/main/java/org/prog/web/HomeWork2.java
@@ -0,0 +1,38 @@
+package org.prog.web;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class HomeWork2 {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        try {
+            driver.get("https://allo.ua/");
+            WebElement profileBtn = driver.findElement(By.className("mh-profile"));
+            profileBtn.click();
+
+            // WebElement loginBtn = driver.findElement(By.className("auth__enter-password"));
+            //  loginBtn.click();
+
+            WebDriverWait driverWait = new WebDriverWait(driver, Duration.ofSeconds(3L));
+            WebElement pwdBtnWait = driverWait.until(
+                    ExpectedConditions.presenceOfElementLocated(By.className("auth__enter-password")));
+            pwdBtnWait.click();
+
+            WebElement phone = driver.findElement(By.className("phoneEmail"));
+            phone.click();
+        } finally {
+            driver.quit();
+        }
+
+
+    }
+}
+
+
--- src/test/java/org/prog/work/HomeWork.java
+++ src/test/java/org/prog/work/HomeWork.java
@@ -4,7 +4,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
 
 public class HomeWork {
 
@@ -12,12 +14,27 @@ public class HomeWork {
     @BeforeSuite
     public void setUp (){
         this.driver = new ChromeDriver();
+        driver.manage().window().maximize();
     }
+    @BeforeMethod
+    public void goAlloUA(){
+        Assert.assertNotNull(driver,"Driver has not been initialized");
+        driver.get("тут повинна бути силка на сайт");
+    }
+
+
+
     @AfterSuite
     public void tearDown(){
         Assert.assertNotNull(driver,"Driver has not been initialized");
    driver.quit();
     }
+
+    @Test
+    public void testAlloUA(){
+
+    }
+
 }
 
 
